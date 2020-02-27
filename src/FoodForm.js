467659d6fcaf6c84b2bd33dc87b031e2f2766f1d
alonzno <alonzoa@cs.uoregon.edu>
2020-02-27 12:55:36 -0800

    Make initial Grid of components

--- src/FoodForm.js
+++ src/FoodForm.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/Styles';
+import Paper from '@material-ui/core/Paper';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function FoodForm() {
-  return (
-    <form noValidate auto-complete="off">
-      <Input />
+  const classes = useStyles();
 
+  return (
+    <form className={classes.root} noValidate auto-complete="off">
+      <Input defaultValue="Name"/>
+      <Input defaultValue="Calories"/>
+      <Input defaultValue="Protein"/>
+      <Input defaultValue="Carbs"/>
+      <Input defaultValue="Fat"/>
+      <Button
+      	variant="contained"
+      	color="primary">
+      	Add
+      </Button>
     </form>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
 import FoodTable from './FoodTable.js';
 import FoodForm from './FoodForm.js';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <FoodTable />
-      <FoodForm />
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <Grid item>
+          <FoodTable />
+        </Grid>
+        <Grid item>
+          <Paper className={classes.paper} elevation={3}>
+            <FoodForm />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
