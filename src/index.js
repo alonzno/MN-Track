117cf5c54a6ad50470c84b6eb4216b0e73710dcd
alonzno <alonzoa@cs.uoregon.edu>
2020-03-01 00:15:06 -0800

    Fix bug with async row updates

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function App(props) {
 
 
   const classes = useStyles();
-  const [rows, setRows] = useState([]);
+  const [rows, setRows] = useState(loadData());
 
     // const rows = [
     //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
@@ -44,13 +44,14 @@ function App(props) {
     //   createData('Cupcake', 305, 3.7, 67, 4.3),
     //   createData('Gingerbread', 356, 16.0, 49, 3.9),
     // ];
-  useEffect(() => (setRows(loadData())), []);
+  useEffect(() => {
+                    ls.set("mn-tracker-data", rows)
+                  }, [ rows ]);
 
 
   function addNewItem(data) {
     setRows(rows.concat(data));
-    ls.set("mn-tracker-data", rows);
-    alert(ls.get("mn-tracker-data"));
+    
     //alert(rows);
   }
 
